cb25b984edea31e6ccb7fa4534c9c32eb7831ca3
xoHalara <jaiimilaxo@gmail.com>
2025-03-17 14:24:20 -0500

    Set up database connection and created migration

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 # import flask migrate here
+from flask_migrate import Migrate 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 # Instantiate Flask-Migrate library here
+migrate = Migrate(app, db)
 
 # Flask-Login login manager
 login_manager = LoginManager()
